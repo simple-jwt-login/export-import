ee7d460fa90ed315ec130739efd854f7219168f5
nicumicle <20170987+nicumicleI@users.noreply.github.com>
2023-03-04 12:09:47 +0100

    initial release

---
+++ README.md
@@ -0,0 +1,29 @@
+# Simple-JWT-Login Export-Import Addon
+
+This addon allows you to import or export your Simple-JWT-Login plugin settings.
+
+This can be usefull when you have multiple websites, and you want to have the same configuration on all your WordPress websites.
+
+| :warning: For security reaseons, It would be better to use different JWT decryption keys for each website. |
+|------------------------------------------------------------------------------------------------------------|
+
+
+## Installation 
+
+Here's how you install and activate the plugin:
+
+1. Download the Simple-JWT-Login-Export-Import plugin.
+2. Upload the .zip file in your WordPress plugin directory.
+3. Activate the plugin from the "Plugins" menu in WordPress.
+
+or
+
+1. Go to the 'Plugins' menu in WordPress and click 'Add New'
+2. Search for 'Simple-JWT-Login-Export-Import' and select 'Install Now'
+3. Activate the plugin when prompted
+
+## Copyright
+
+This project is distributed under the [GNU General Public License v3.0](https://github.com/simple-jwt-login/export-import/blob/main/LICENSE).
+
+By submitting a pull request to this project, you agree to license your contribution under the GNU General Public License v3.0 to this project.
---
+++ views/layout.php
@@ -0,0 +1,112 @@
+<?php
+
+use SimpleJWTLogin\Modules\SimpleJWTLoginSettings;
+use SimpleJWTLogin\Modules\WordPressData;
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+$value = '';
+$error = null;
+$success = null;
+$wordPressData = new WordPressData();
+
+if (!empty($_POST) && isset($_POST['action']) && isset($_POST['_wpnonce'])) {
+    $result = $wordPressData
+        ->checkNonce($_POST['_wpnonce'], WordPressData::NONCE_NAME);
+    if ($result === false) {
+        $error = 'Something is wrong.';
+        $success = null;
+        $value = '';
+    } else {
+        $action = esc_html($_POST['action']);
+        switch ($action) {
+            case "import":
+                if (isset($_POST['settings'])) {
+                    $settings = base64_decode(esc_html($_POST['settings']));
+                    if ($settings === false) {
+                        $error = 'Invalid import settings';
+                        break;
+                    }
+                    $array = json_decode($settings, true);
+                    if (!is_array($array)) {
+                        $error = 'Invalid import settings.';
+                        break;
+                    }
+
+                    $needUpdate = $wordPressData->getOptionFromDatabase(SimpleJWTLoginSettings::OPTIONS_KEY) !== false;
+
+                    if ($needUpdate) {
+                        $wordPressData->updateOption(SimpleJWTLoginSettings::OPTIONS_KEY, $settings);
+                        $success = 'Settings has been imported.';
+                        break;
+                    }
+
+                    $wordPressData->addOption(SimpleJWTLoginSettings::OPTIONS_KEY, $settings);
+                    $success = 'Settings has been imported.';
+                    break;
+                }
+                break;
+
+            case 'export':
+                $value = base64_encode($wordPressData->getOptionFromDatabase(SimpleJWTLoginSettings::OPTIONS_KEY));
+                break;
+        }
+    }
+}
+?>
+<div id="simple-jwt-login-export-import">
+    <?php
+    if ($success !== null || $error !== null) {
+
+        $alertMessage = $success;
+        $type = 'success';
+        if ($error !== null) {
+            $type = 'danger';
+            $alertMessage = $error;
+        }
+
+        ?>
+        <div class="row">
+            <div class="alert alert-<?php echo $type; ?>">
+                <?php echo esc_html($alertMessage); ?>
+            </div>
+        </div>
+        <?php
+    }
+    ?>
+    <div class="row">
+        <div class="col-md-12">
+            <h1><?php echo __('Export/Import Settings ', 'simple-jwt-login-export-import'); ?></h1>
+        </div>
+    </div>
+    <form method="POST">
+        <?php
+        $wordPressData->insertNonce(WordPressData::NONCE_NAME);
+        ?>
+        <div class="row">
+            <div class="col-md-12">
+                <input type="submit" name="action" value="import" class="btn btn-dark"/>
+                <input type="submit" name="action" value="export" class="btn btn-dark"/>
+            </div>
+        </div>
+        <hr/>
+
+        <div class="row">
+            <div class="col-md-12">
+                <label for="settings-response">Response:</label>
+                <textarea
+                        id="settings-response"
+                        rows="20"
+                        name="settings"
+                        class="input form-control"
+                    <?php if (!empty($value)) {
+                        echo "readonly";
+                    }
+                    ?>
+            ><?php echo esc_html($value); ?></textarea>
+            </div>
+        </div>
+    </form>
+</div>
